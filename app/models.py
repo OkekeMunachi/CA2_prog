2f8abd3c89df2ac679f6dc4ff76edd6ceedee4e8
Munachi <okekemunachipraise@gmail.com>
2026-07-14 23:37:21 +0100

    Add Vulnerability model
    Source: Self

--- app/models.py
+++ app/models.py
@@ -28,3 +28,34 @@ class Issue(db.Model):
 
     def __repr__(self):
         return f"<Issue {self.title}>"
+
+class Vulnerability(db.Model):
+    __tablename__ = "vulnerabilities"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    title = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    description = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    severity = db.Column(
+        db.String(20),
+        default="Medium"
+    )
+
+    cve_id = db.Column(
+        db.String(50),
+        nullable=True
+    )
+
+    def __repr__(self):
+        return f"<Vulnerability {self.title}>"
